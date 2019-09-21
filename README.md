b4691a20b59990fffc17acc109cb9e1919716358
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2019-09-21 22:56:39 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,24 @@
+# OpenWebProject HTTP
+
+**React loading bar for owp.http**
+
+Uses [react.loading-bar](https://www.npmjs.com/package/react-loading-bar) to signal useage of [owp.http](https://github.com/AndreasArvidsson/OpenWebProject-HTTP)
+
+## Usage
+```javascript
+import LoadingBar from "owp.http-loading-bar"
+
+<LoadingBar />
+```
+
+## Options
+All options from [react.loading-bar](https://www.npmjs.com/package/react-loading-bar) is available.
+
+```javascript
+<LoadingBar
+    show={true}
+    color="red"
+    change={false}
+    showSpinner={true}
+/>
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+/**
+ * @author Andreas Arvidsson
+ * https://github.com/AndreasArvidsson/OpenWebProject-HTTP-loading-bar
+ */
+import "react-loading-bar/dist/index.css";
+import React, { useState, useEffect } from "react";
+import ReactLoadingBar from "react-loading-bar";
+import HTTP from "owp.http";
+
+let countStarted = 0;
+let countFinished = 0;
+let isLoading = false;
+let intervalId;
+
+const HTTPLoadingBar = (props) => {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        HTTP.setOnStateChange(readyState => {
+            switch (readyState) {
+                case 1:
+                    ++countStarted;
+                    if (!isLoading) {
+                        startLoadingBar();
+                    }
+                    break;
+                case 4:
+                    ++countFinished;
+                    if (countFinished >= countStarted) {
+                        stopLoadingBar();
+                    }
+            }
+        });
+    }, []);
+
+    const startLoadingBar = () => {
+        isLoading = true;
+        progressLoadingBar();
+        intervalId = setInterval(progressLoadingBar, 100);
+    }
+
+    const progressLoadingBar = () => {
+        if (isLoading) {
+            setShow(true);
+        }
+    }
+
+    const stopLoadingBar = () => {
+        isLoading = false;
+        clearInterval(intervalId);
+        setShow(false);
+    }
+
+    //return <ReactLoadingBar show={show} {...props} />
+    return React.createElement(ReactLoadingBar, { show, ...props }, null);
+}
+export default HTTPLoadingBar;
