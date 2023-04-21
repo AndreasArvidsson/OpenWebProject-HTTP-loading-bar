6dd00b74d9532aa272f47088456fafbcc96fab61
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-04-21 11:16:38 +0200

    Added declarations file

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+interface Options{
+    className: string
+    classNameInner: string
+}
+declare const   HTTPLoadingBar:({ className, classNameInner }: Options)  =>  JSX.Element
+export default HTTPLoadingBar;
