7585ebce2eda47b94c86c4922f7939dda3444ee8
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-08-10 02:13:42 +0200

    Rename index.js to index.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,9 @@ const ESLintPlugin = require("eslint-webpack-plugin");
 const path = require("path");
 
 module.exports = (env, argv) => {
-
-    console.log("----------------------------")
+    console.log("----------------------------");
     console.log(" ", argv.mode);
-    console.log("----------------------------\n")
+    console.log("----------------------------\n");
 
     const res = {
         entry: path.resolve(__dirname, "demo/test.js"),
@@ -20,44 +19,44 @@ module.exports = (env, argv) => {
                             loader: "babel-loader",
                             options: {
                                 presets: [
-                                    ["@babel/preset-react", {
-                                        //Replaces the import source when importing functions.
-                                        //Remove for @babel/core 8
-                                        runtime: "automatic"
-                                    }]
-                                ]
-                            }
-                        }
-                    ]
+                                    [
+                                        "@babel/preset-react",
+                                        {
+                                            //Replaces the import source when importing functions.
+                                            //Remove for @babel/core 8
+                                            runtime: "automatic",
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    ],
                 },
                 {
                     test: /\.css$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader"
-                    ]
-                }
-            ]
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+            ],
         },
         plugins: [
             new HtmlWebPackPlugin({
-                template: path.resolve(__dirname, "demo/index.html")
+                template: path.resolve(__dirname, "demo/index.html"),
             }),
             //Extract css styles as external file.
             new MiniCssExtractPlugin({
-                filename: "styles.css"
+                filename: "styles.css",
             }),
             //Lint JS files
             new ESLintPlugin({
-                overrideConfigFile: path.resolve(__dirname, "eslintrc.js")
-            })
-        ]
+                overrideConfigFile: path.resolve(__dirname, "eslintrc.js"),
+            }),
+        ],
     };
 
     if (argv.mode === "production") {
         res.output = {
             path: path.resolve(__dirname, "docs"),
-            filename: "index.js"
+            filename: "index.jsx",
         };
     }
 
